aadee48eb69377caf2c6b842133cade5078ed104
mercedesalemany <mercedesalemany@gmail.com>
2024-03-26 22:14:41 +0100

    Este es el segundo commit

--- puntoInicio.py
+++ puntoInicio.py
@@ -1,2 +1,3 @@
 print("Este es el primer archivo del git")
 print("Punto de partida, primer archivo y primera version") #Con comentario para que esté bien completo
+print("Ahora modifico estos datos, qué pasa¿?")
