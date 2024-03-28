c400f954e6a328be5090249b61d754fd28ba4014
mercedesalemany <mercedesalemany@gmail.com>
2024-03-28 11:33:46 +0100

    Comiteo el cambio modificado en stash

--- puntoInicio.py
+++ puntoInicio.py
@@ -1,3 +1,4 @@
 print("Este es el primer archivo del git")
 print("Punto de partida, primer archivo y primera version") #Con comentario para que esté bien completo
 print("Ahora modifico estos datos, qué pasa¿?")
+print("cORREGIO ")
